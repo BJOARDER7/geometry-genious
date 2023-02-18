0b64ba9aab8631ca4a0aa1b53ab85d139915a0e6
BJoarder <bjoarder87@gmail.com>
2023-02-18 10:38:55 +0600

    js common function added and linked

--- js/geometry.js
+++ js/geometry.js
@@ -86,37 +86,3 @@ document.getElementById('ellipse-calc-btn').addEventListener('click', function()
    serialNo += 1;
   const ellipseOutput = createOutput('calculation-output', ellipse, ellipseArea);  
 });
-
-// function for getting number value from input field by using id
-function getInputTagValue(id){
-  const getValueString = document.getElementById(id).value;
-  const getValue = parseInt(getValueString);
-  return getValue;
-}
-// function for getting number value from h2 field by using id
-function getStringTagValue(id){
-  const getValueString = document.getElementById(id).innerText;
-  const getValue = parseInt(getValueString);
-  return getValue;
-}
-// get arae name
-function getAreaName(id){
-  const areaName = document.getElementById(id).innerText;
-  return areaName;
-}
-
-// calculation output function
-function createOutput(id,geometry,triangleArea){
-const calculationOutput = document.getElementById(id);
-  const ul = document.createElement('ul');
-  ul.innerHTML = `
-    <tr class="my-2">
-      <td>${serialNo}.</td>
-      <td>${geometry}</td>
-      <td">${triangleArea}cm<sup>2</sup></td>
-      <td"><button class="bg-blue-500 rounded px-1 py-1 my-2">Convert to m<sup>2</sup></button></td>
-    
-    </tr>
-  `
-  calculationOutput.appendChild(ul);
-}
